3e8165e64883cfdcf1938588c02a3bf5d8258815
YunO <fish970907@naver.com>
2023-03-20 17:30:04 +0900

    MyCommit 2 그레이스케일 변환 기능 추가

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import cv2
 src = cv2.imread('c:/photo/picture01.jpg')
 
 
-
+## 영상처리/컴퓨터 비전 알고리즘 적용##
+dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 
 
 
@@ -15,7 +16,7 @@ src = cv2.imread('c:/photo/picture01.jpg')
 
 # 영상 디스플레이
 cv2.imshow('src', src)
-
+cv2.imshow('dst1', dst1)
 
 # 마무리
 cv2.waitKey(0)
